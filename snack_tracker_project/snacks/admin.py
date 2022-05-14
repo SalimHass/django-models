49dc4232185ecfb2e96ec8b20d772c62d26380c9
Salim Hassouneh <salim.hassouneh@gmail.com>
2022-05-14 21:52:19 +0300

    lab 27

---
+++ snack_tracker_project/snacks/admin.py
@@ -0,0 +1,12 @@
+from unicodedata import name
+from django.contrib import admin
+from .models import Snack
+
+
+# Register your models here.
+"""
+@admin.register(Snack)
+class AdminSnack(admin.ModelAdmin):
+    list_display= ['name', 'purchaser']"""
+
+admin.site.register(Snack)
